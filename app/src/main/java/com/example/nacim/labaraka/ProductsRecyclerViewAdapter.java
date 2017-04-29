1ba7217804699a2c3291fee6f9e15b0f21a3c147
Nacim Arrahmane <narrahmane@gmail.com>
2017-04-29 19:40:57 +0200

    [Class][Layout][ProductsRecyclerViewAdapter][product_grid_cell] Adapted height of images to math 1/2 of the screen.

--- app/src/main/java/com/example/nacim/labaraka/ProductsRecyclerViewAdapter.java
+++ app/src/main/java/com/example/nacim/labaraka/ProductsRecyclerViewAdapter.java
@@ -3,6 +3,7 @@ package com.example.nacim.labaraka;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Typeface;
+import android.support.v4.content.res.ResourcesCompat;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -30,13 +31,17 @@ public class ProductsRecyclerViewAdapter extends RecyclerView.Adapter<ProductsRe
     @Override
     public ProductViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View productView = layoutInflater.inflate(R.layout.product_grid_cell, parent, false);
-        productView.findViewById(R.id.product_grid_cell_textview).setMinimumHeight(parent.getMeasuredHeight() / 2);
+
+        ImageView productImageView = (ImageView) productView.findViewById(R.id.product_grid_cell_imageview);
+        productImageView.setMinimumHeight(parent.getMeasuredHeight() / 2);
+        productImageView.setMaxHeight(parent.getMeasuredHeight() / 2);
+
         return new ProductViewHolder(productView);
     }
 
     @Override
     public void onBindViewHolder(final ProductViewHolder holder, final int position) {
-        holder.productTextView.setImageDrawable(context.getDrawable(R.drawable.jupe));
+        holder.productImageView.setImageDrawable(ResourcesCompat.getDrawable(context.getResources(), R.drawable.jupe, null));
         holder.nameTextView.setText("Mini jupe short à fleur");
         //holder.productTextView.setBackgroundColor(context.getColor(R.color.colorAccent));
         /* DATA MANAGER */
@@ -57,14 +62,14 @@ public class ProductsRecyclerViewAdapter extends RecyclerView.Adapter<ProductsRe
 
     public class ProductViewHolder extends RecyclerView.ViewHolder {
 
-        private ImageView productTextView;
+        private ImageView productImageView;
         private TextView newTextView;
         private TextView nameTextView;
         private TextView priceTextView;
 
         public ProductViewHolder(View itemView) {
             super(itemView);
-            productTextView = (ImageView) itemView.findViewById(R.id.product_grid_cell_textview);
+            productImageView = (ImageView) itemView.findViewById(R.id.product_grid_cell_imageview);
             newTextView = (TextView) itemView.findViewById(R.id.product_new_textview);
             nameTextView = (TextView) itemView.findViewById(R.id.product_name_textview);
             priceTextView = (TextView) itemView.findViewById(R.id.product_price_textview);
